2f2cfd13bd5da2e73cffbdbedc295ed3bf40792f
Xenia Radulovic <radulovic.xeniaa@gmail.com>
2022-04-15 16:26:35 +0200

    added weaons to the list page

---
+++ js/list/weapons.js
@@ -0,0 +1,45 @@
+
+//Skins API
+
+fetch("https://valorant-api.com/v1/weapons")
+    .then(response => response.json())
+    .then(data => {
+        let weapons = data.data;
+
+
+        //how to display all weapons
+        //making skin cards
+        weapons.forEach((weapon) => {
+            let card = document.createElement("div");
+            card.classList.add('card', weapon.shopData.category);
+            //weapon name
+            let weaponName = document.createElement("h3")
+            weaponName.innerHTML = weapon.displayName;
+            card.appendChild(weaponName);
+            //skin image
+            let imgContainer = document.createElement("div");
+            imgContainer.classList.add('image-container');
+            let image = document.createElement("img");
+            image.setAttribute("src", weapon.displayIcon);
+            imgContainer.appendChild(image);
+            card.appendChild(imgContainer);
+            //skin category
+            let gunCategory = document.createElement("div");
+            gunCategory.classList.add('a-category');
+            let gunCategoryText = document.createElement("span");
+            gunCategoryText.innerHTML = "Category: " + weapon.shopData.category;
+            gunCategory.appendChild(gunCategoryText);
+            card.appendChild(gunCategory);
+            //gun price
+            let gunPrice = document.createElement("div");
+            gunPrice.classList.add('a-price');
+            let gunPriceText = document.createElement("span");
+            gunPriceText.innerHTML = weapon.shopData.cost;
+            gunPrice.appendChild(gunPriceText);
+            card.appendChild(gunPrice);
+            //add agent to html
+            document.getElementById('weapons').appendChild(card);
+        })
+
+    
+    })
